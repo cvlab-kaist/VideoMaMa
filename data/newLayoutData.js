63be38027e5c77a42564317864ce925a4e9ff0c8
Sangbeom Lim <limsbeom@korea.ac.kr>
2026-01-19 17:57:45 +0900

    Initial project page setup

---
+++ data/newLayoutData.js
@@ -0,0 +1,33 @@
+    const newLayoutData = [
+        {
+            "input_rgb": "comparison/ed4e023a_rgb.mp4",
+            "input_mask": "comparison/ed4e023a_mask.mp4",
+            "models": [
+                { "alpha": "comparison/MaGGIe/ed4e023a_pred_alpha.mp4", "comp": "comparison/MaGGIe/ed4e023a_pred_greenscreen.mp4" },
+                { "alpha": "comparison/videomama/ed4e023a_pred_alpha.mp4", "comp": "comparison/videomama/ed4e023a_pred_greenscreen.mp4" },
+                { "alpha": "comparison/MatAnyone/ed4e023a_pred_alpha.mp4", "comp": "comparison/MatAnyone/ed4e023a_pred_greenscreen.mp4" },
+                { "alpha": "comparison/mav_mask/ed4e023a_pred_alpha.mp4", "comp": "comparison/mav_mask/ed4e023a_pred_greenscreen.mp4" },
+            ]
+        },
+        {
+            "input_rgb": "comparison/karina_rgb.mp4",
+            "input_mask": "comparison/karina_mask.mp4",
+            "models": [
+                { "alpha": "comparison/MaGGIe/karina_pred_alpha.mp4", "comp": "comparison/MaGGIe/karina_pred_greenscreen.mp4" },
+                { "alpha": "comparison/videomama/karina_pred_alpha.mp4", "comp": "comparison/videomama/karina_pred_greenscreen.mp4" },
+                { "alpha": "comparison/MatAnyone/karina_pred_alpha.mp4", "comp": "comparison/MatAnyone/karina_pred_greenscreen.mp4" },
+                { "alpha": "comparison/mav_mask/karina_pred_alpha.mp4", "comp": "comparison/mav_mask/karina_pred_greenscreen.mp4" },
+            ]
+        },
+        {
+            "input_rgb": "comparison/basket_rgb.mp4",
+            "input_mask": "comparison/basket_mask.mp4",
+            "models": [
+                { "alpha": "comparison/MaGGIe/basket_pred_alpha.mp4", "comp": "comparison/MaGGIe/basket_pred_greenscreen.mp4" },
+                { "alpha": "comparison/videomama/basket_pred_alpha.mp4", "comp": "comparison/videomama/basket_pred_greenscreen.mp4" },
+                { "alpha": "comparison/MatAnyone/basket_pred_alpha.mp4", "comp": "comparison/MatAnyone/basket_pred_greenscreen.mp4" },
+                { "alpha": "comparison/mav_mask/basket_pred_alpha.mp4", "comp": "comparison/mav_mask/basket_pred_greenscreen.mp4" },
+            ]
+        },
+
+    ];
---
+++ data/videoData.js
@@ -0,0 +1,425 @@
+    const videoData = [
+        {
+            "id": 1,
+            "title": "# 1",
+            "ref": "mav_sav/0_sav_036684_manual_1_rgb.mp4",
+            "result1": "mav_sav/0_sav_036684_manual_1_mask.mp4",
+            "input1": "mav_sav/0_sav_036684_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_036684_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_036684_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 2,
+            "title": "# 2",
+            "ref": "mav_sav/0_sav_037832_manual_0_rgb.mp4",
+            "result1": "mav_sav/0_sav_037832_manual_0_mask.mp4",
+            "input1": "mav_sav/0_sav_037832_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_037832_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_037832_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 3,
+            "title": "# 3",
+            "ref": "mav_sav/0_sav_038251_manual_2_rgb.mp4",
+            "result1": "mav_sav/0_sav_038251_manual_2_mask.mp4",
+            "input1": "mav_sav/0_sav_038251_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_038251_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_038251_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 4,
+            "title": "# 4",
+            "ref": "mav_sav/0_sav_040852_manual_0_rgb.mp4",
+            "result1": "mav_sav/0_sav_040852_manual_0_mask.mp4",
+            "input1": "mav_sav/0_sav_040852_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_040852_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_040852_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 5,
+            "title": "# 5",
+            "ref": "mav_sav/0_sav_040974_manual_5_rgb.mp4",
+            "result1": "mav_sav/0_sav_040974_manual_5_mask.mp4",
+            "input1": "mav_sav/0_sav_040974_manual_5_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_040974_manual_5_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_040974_manual_5_pred_greenscreen.mp4"
+        },
+        {
+            "id": 6,
+            "title": "# 6",
+            "ref": "mav_sav/0_sav_042715_manual_0_rgb.mp4",
+            "result1": "mav_sav/0_sav_042715_manual_0_mask.mp4",
+            "input1": "mav_sav/0_sav_042715_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_042715_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_042715_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 7,
+            "title": "# 7",
+            "ref": "mav_sav/0_sav_046702_manual_0_rgb.mp4",
+            "result1": "mav_sav/0_sav_046702_manual_0_mask.mp4",
+            "input1": "mav_sav/0_sav_046702_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_046702_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_046702_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 8,
+            "title": "# 8",
+            "ref": "mav_sav/0_sav_047034_manual_1_rgb.mp4",
+            "result1": "mav_sav/0_sav_047034_manual_1_mask.mp4",
+            "input1": "mav_sav/0_sav_047034_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_047034_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_047034_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 9,
+            "title": "# 9",
+            "ref": "mav_sav/0_sav_047229_manual_2_rgb.mp4",
+            "result1": "mav_sav/0_sav_047229_manual_2_mask.mp4",
+            "input1": "mav_sav/0_sav_047229_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/0_sav_047229_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/0_sav_047229_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 10,
+            "title": "# 10",
+            "ref": "mav_sav/sav_036144_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_036144_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_036144_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_036144_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_036144_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 11,
+            "title": "# 11",
+            "ref": "mav_sav/sav_036646_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_036646_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_036646_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_036646_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_036646_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 12,
+            "title": "# 12",
+            "ref": "mav_sav/sav_036854_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_036854_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_036854_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_036854_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_036854_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 13,
+            "title": "# 13",
+            "ref": "mav_sav/sav_037303_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_037303_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_037303_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_037303_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_037303_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 14,
+            "title": "# 14",
+            "ref": "mav_sav/sav_037646_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_037646_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_037646_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_037646_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_037646_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 15,
+            "title": "# 15",
+            "ref": "mav_sav/sav_037892_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_037892_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_037892_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_037892_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_037892_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 16,
+            "title": "# 16",
+            "ref": "mav_sav/sav_037925_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_037925_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_037925_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_037925_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_037925_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 17,
+            "title": "# 17",
+            "ref": "mav_sav/sav_038109_manual_3_rgb.mp4",
+            "result1": "mav_sav/sav_038109_manual_3_mask.mp4",
+            "input1": "mav_sav/sav_038109_manual_3_pred_alpha.mp4",
+            "result2": "mav_sav/sav_038109_manual_3_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_038109_manual_3_pred_greenscreen.mp4"
+        },
+        {
+            "id": 18,
+            "title": "# 18",
+            "ref": "mav_sav/sav_038224_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_038224_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_038224_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_038224_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_038224_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 19,
+            "title": "# 19",
+            "ref": "mav_sav/sav_038251_manual_3_rgb.mp4",
+            "result1": "mav_sav/sav_038251_manual_3_mask.mp4",
+            "input1": "mav_sav/sav_038251_manual_3_pred_alpha.mp4",
+            "result2": "mav_sav/sav_038251_manual_3_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_038251_manual_3_pred_greenscreen.mp4"
+        },
+        {
+            "id": 20,
+            "title": "# 20",
+            "ref": "mav_sav/sav_038469_manual_3_rgb.mp4",
+            "result1": "mav_sav/sav_038469_manual_3_mask.mp4",
+            "input1": "mav_sav/sav_038469_manual_3_pred_alpha.mp4",
+            "result2": "mav_sav/sav_038469_manual_3_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_038469_manual_3_pred_greenscreen.mp4"
+        },
+        {
+            "id": 21,
+            "title": "# 21",
+            "ref": "mav_sav/sav_038994_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_038994_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_038994_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_038994_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_038994_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 22,
+            "title": "# 22",
+            "ref": "mav_sav/sav_039607_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_039607_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_039607_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_039607_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_039607_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 23,
+            "title": "# 23",
+            "ref": "mav_sav/sav_039607_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_039607_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_039607_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_039607_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_039607_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 24,
+            "title": "# 24",
+            "ref": "mav_sav/sav_039876_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_039876_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_039876_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_039876_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_039876_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 25,
+            "title": "# 25",
+            "ref": "mav_sav/sav_039898_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_039898_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_039898_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_039898_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_039898_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 26,
+            "title": "# 26",
+            "ref": "mav_sav/sav_040142_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_040142_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_040142_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_040142_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_040142_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 27,
+            "title": "# 27",
+            "ref": "mav_sav/sav_040506_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_040506_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_040506_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_040506_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_040506_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 28,
+            "title": "# 28",
+            "ref": "mav_sav/sav_040974_manual_4_rgb.mp4",
+            "result1": "mav_sav/sav_040974_manual_4_mask.mp4",
+            "input1": "mav_sav/sav_040974_manual_4_pred_alpha.mp4",
+            "result2": "mav_sav/sav_040974_manual_4_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_040974_manual_4_pred_greenscreen.mp4"
+        },
+        {
+            "id": 29,
+            "title": "# 29",
+            "ref": "mav_sav/sav_042054_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_042054_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_042054_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_042054_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_042054_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 30,
+            "title": "# 30",
+            "ref": "mav_sav/sav_042500_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_042500_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_042500_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_042500_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_042500_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 31,
+            "title": "# 31",
+            "ref": "mav_sav/sav_042551_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_042551_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_042551_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_042551_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_042551_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 32,
+            "title": "# 32",
+            "ref": "mav_sav/sav_042715_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_042715_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_042715_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_042715_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_042715_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 33,
+            "title": "# 33",
+            "ref": "mav_sav/sav_043360_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_043360_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_043360_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_043360_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_043360_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 34,
+            "title": "# 34",
+            "ref": "mav_sav/sav_043471_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_043471_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_043471_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_043471_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_043471_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 35,
+            "title": "# 35",
+            "ref": "mav_sav/sav_043471_manual_2_rgb.mp4",
+            "result1": "mav_sav/sav_043471_manual_2_mask.mp4",
+            "input1": "mav_sav/sav_043471_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/sav_043471_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_043471_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 36,
+            "title": "# 36",
+            "ref": "mav_sav/sav_043477_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_043477_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_043477_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_043477_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_043477_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 37,
+            "title": "# 37",
+            "ref": "mav_sav/sav_043523_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_043523_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_043523_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_043523_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_043523_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 38,
+            "title": "# 38",
+            "ref": "mav_sav/sav_044015_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_044015_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_044015_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_044015_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_044015_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 39,
+            "title": "# 39",
+            "ref": "mav_sav/sav_044725_manual_0_rgb.mp4",
+            "result1": "mav_sav/sav_044725_manual_0_mask.mp4",
+            "input1": "mav_sav/sav_044725_manual_0_pred_alpha.mp4",
+            "result2": "mav_sav/sav_044725_manual_0_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_044725_manual_0_pred_greenscreen.mp4"
+        },
+        {
+            "id": 40,
+            "title": "# 40",
+            "ref": "mav_sav/sav_044727_manual_2_rgb.mp4",
+            "result1": "mav_sav/sav_044727_manual_2_mask.mp4",
+            "input1": "mav_sav/sav_044727_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/sav_044727_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_044727_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 41,
+            "title": "# 41",
+            "ref": "mav_sav/sav_045102_manual_2_rgb.mp4",
+            "result1": "mav_sav/sav_045102_manual_2_mask.mp4",
+            "input1": "mav_sav/sav_045102_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/sav_045102_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_045102_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 42,
+            "title": "# 42",
+            "ref": "mav_sav/sav_045971_manual_2_rgb.mp4",
+            "result1": "mav_sav/sav_045971_manual_2_mask.mp4",
+            "input1": "mav_sav/sav_045971_manual_2_pred_alpha.mp4",
+            "result2": "mav_sav/sav_045971_manual_2_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_045971_manual_2_pred_greenscreen.mp4"
+        },
+        {
+            "id": 43,
+            "title": "# 43",
+            "ref": "mav_sav/sav_045980_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_045980_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_045980_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_045980_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_045980_manual_1_pred_greenscreen.mp4"
+        },
+        {
+            "id": 44,
+            "title": "# 44",
+            "ref": "mav_sav/sav_046013_manual_4_rgb.mp4",
+            "result1": "mav_sav/sav_046013_manual_4_mask.mp4",
+            "input1": "mav_sav/sav_046013_manual_4_pred_alpha.mp4",
+            "result2": "mav_sav/sav_046013_manual_4_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_046013_manual_4_pred_greenscreen.mp4"
+        },
+        {
+            "id": 45,
+            "title": "# 45",
+            "ref": "mav_sav/sav_046013_manual_5_rgb.mp4",
+            "result1": "mav_sav/sav_046013_manual_5_mask.mp4",
+            "input1": "mav_sav/sav_046013_manual_5_pred_alpha.mp4",
+            "result2": "mav_sav/sav_046013_manual_5_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_046013_manual_5_pred_greenscreen.mp4"
+        },
+        {
+            "id": 46,
+            "title": "# 46",
+            "ref": "mav_sav/sav_046104_manual_3_rgb.mp4",
+            "result1": "mav_sav/sav_046104_manual_3_mask.mp4",
+            "input1": "mav_sav/sav_046104_manual_3_pred_alpha.mp4",
+            "result2": "mav_sav/sav_046104_manual_3_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_046104_manual_3_pred_greenscreen.mp4"
+        },
+        {
+            "id": 47,
+            "title": "# 47",
+            "ref": "mav_sav/sav_046893_manual_1_rgb.mp4",
+            "result1": "mav_sav/sav_046893_manual_1_mask.mp4",
+            "input1": "mav_sav/sav_046893_manual_1_pred_alpha.mp4",
+            "result2": "mav_sav/sav_046893_manual_1_mask_greenscreen.mp4",
+            "input2": "mav_sav/sav_046893_manual_1_pred_greenscreen.mp4"
+        },
+    ];
